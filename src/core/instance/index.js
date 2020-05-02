7e565cc909d94d2032485a21f8132a997f7e874f
zhourongjing <zhourongjing@ppdai.com>
2020-05-02 11:48:09 +0800

    first commit

---
+++ src/core/instance/index.js
@@ -0,0 +1,26 @@
+import { initMixin } from './init'
+import { stateMixin } from './state'
+import { renderMixin } from './render'
+import { eventsMixin } from './events'
+import { lifecycleMixin } from './lifecycle'
+import { warn } from '../util/index'
+
+// Vue的构造函数
+function Vue (options) {
+  if (process.env.NODE_ENV !== 'production' &&
+    !(this instanceof Vue)
+  ) {
+    warn('Vue is a constructor and should be called with the `new` keyword')
+  }
+  // 初始化做了什么？
+  // init是哪来的
+  this._init(options)
+}
+
+initMixin(Vue) // 实现init方法
+stateMixin(Vue)
+eventsMixin(Vue)
+lifecycleMixin(Vue)
+renderMixin(Vue)
+
+export default Vue
